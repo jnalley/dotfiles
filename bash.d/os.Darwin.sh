a0606c49e15e7dbe374f16f3f5bac8877acab67e
Jon Nalley <jnalley@truecar.com>
2014-11-15 12:30:19 -0600

    use gnu versions of certain commands on os x when they are available

--- bash.d/os.Darwin.sh
+++ bash.d/os.Darwin.sh
@@ -2,3 +2,12 @@
 
 # enable color ls output
 export CLICOLOR=1
+
+# use gnu versions if they are available (brew install coreutils)
+for CMD in gcp gmv grm gmkdir gmd5sum gsha1sum \
+            gsha224sum gsha256sum gsha384sum gsha512sum; do
+    [[ -x /usr/local/bin/${CMD} ]] && [[ ! -L ${HOME}/local/bin/${CMD} ]] && \
+        ln -s /usr/local/bin/${CMD} ${HOME}/local/bin/${CMD:1}
+done
+
+inpath coreosx && eval $(coreosx env)
