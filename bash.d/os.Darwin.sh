866a37e50c40c23516e3f28fcd29ea902717db07
Jon Nalley <code@bluebot.org>
2015-03-08 09:35:04 -0500

    added vagrant helper functions

--- bash.d/os.Darwin.sh
+++ bash.d/os.Darwin.sh
@@ -4,10 +4,30 @@
 export CLICOLOR=1
 
 # use gnu versions if they are available (brew install coreutils)
-for CMD in gcp gmv grm gmkdir gmd5sum gsha1sum \
+for CMD in gcp gmv grm gmkdir gtar gmd5sum gsha1sum \
             gsha224sum gsha256sum gsha384sum gsha512sum; do
     [[ -x /usr/local/bin/${CMD} ]] && [[ ! -L ${HOME}/local/bin/${CMD} ]] && \
         ln -s /usr/local/bin/${CMD} ${HOME}/local/bin/${CMD:1}
 done
 
 inpath coreosx && eval $(coreosx env)
+
+reset-dns-cache() {
+    sudo discoveryutil mdnsflushcache
+}
+
+# run vagrant commands from anywhere
+# <vagrant vm name> <command>
+__vagrant() {
+    local instance=$1; shift
+    [[ -d ~/Projects/vagrant/${instance} ]] || return 1
+    (cd ~/Projects/vagrant/${instance} ; vagrant $@)
+}
+
+for dir in ${HOME}/Projects/vagrant/*; do
+    [[ -d ${dir} ]] || continue
+    name=${dir##*/} 
+    eval "function ${name} { __vagrant ${name} \$@; };" \
+         "function _${name} { _vagrant \$@; }"
+    complete -F _${name} ${name}
+done
