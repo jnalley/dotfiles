6906a9bdff5465de58d7b35356d3945f710fec32
Jon Nalley <code@bluebot.org>
2015-03-16 14:39:02 -0500

    more tmux tweaks

--- bash.d/tmux.sh
+++ bash.d/tmux.sh
@@ -3,9 +3,11 @@
 # this script should only ever be sourced
 [[ ${BASH_SOURCE[0]} != ${0} ]] || exit 1
 
+################################
+# Automate tmux session creation
+################################
+
 ########################################################################
-# start tmux when logging in via ssh if terminal supports 256 colors
-#
 # Avoid problems with SSH_AUTH_SOCK when using multiple connections to
 # the same host by using SSH connection sharing. Refer to ControlMaster
 # in ssh_config(5).
@@ -17,10 +19,10 @@ TMUXCMD="$(type -p tmux)"
 
 [[ -x ${TMUXCMD} ]] || return
 
-# connect if a sesion exists
+# connect if a sesion exists and has no clients
 ${TMUXCMD} has-session 2> /dev/null && \
-    exec ${TMUXCMD} attach
+    [[ -z $(${TMUXCMD}  list-clients > /dev/null) ]] && \
+        exec ${TMUXCMD} attach
 
-# create a new session when connecting through SSH
-[[ -r ${SSH_TTY} ]] && \
-    exec ${TMUXCMD} new
+# auto-create session only when connecting through SSH
+[[ -r ${SSH_TTY} ]] && exec ${TMUXCMD} new
