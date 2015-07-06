e804d51d1e6f2b843895e05604de2d6ea2a095f2
Jon Nalley <code@bluebot.org>
2015-07-06 11:04:22 -0700

    Fix python REPL history

--- pystartup.py
+++ pystartup.py
@@ -1,3 +1,18 @@
-import rlcompleter
+import os
+import atexit
 import readline
-readline.parse_and_bind("tab: complete")
+
+""" save history - not necessary with bpython """
+if os.path.basename(os.environ['_']) != 'bpython':
+    def save_history(path, readline):
+        readline.write_history_file(path)
+
+    history = os.path.expanduser('~/.pythonhist')
+    atexit.register(save_history, history, readline)
+
+    if os.path.exists(history):
+        readline.read_history_file(history)
+
+    readline.parse_and_bind("tab: complete")
+
+del os, atexit, readline
