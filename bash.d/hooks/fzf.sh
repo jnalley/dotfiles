7955f7cd588b26fb664af10162eaf88b71dec879
Jon Nalley <code@bluebot.org>
2017-11-20 18:03:18 -0600

    Use 'fd' instead of 'rg'

--- bash.d/hooks/fzf.sh
+++ bash.d/hooks/fzf.sh
@@ -3,8 +3,8 @@
 # this script should only ever be sourced
 [[ ${BASH_SOURCE[0]} != ${0} ]] || exit 1
 
-# bail if executable is not present
-[[ -x ${HOME}/.fzf/bin/fzf ]] || return 1
+# bail if executables are not present
+[[ -x ${HOME}/.fzf/bin/fzf ]] && inpath fd || return 1
 
 # add fzf bin directory to path
 [[ "$PATH" == *${HOME}/.fzf/bin* ]] || \
@@ -27,4 +27,23 @@ export FZF_DEFAULT_OPTS='--height 30% --border'
 # --hidden: Search hidden files and folders
 # --follow: Follow symlinks
 # --glob: Additional conditions for search (in this case ignore everything in the .git/ folder)
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+export FZF_CTRL_T_COMMAND="${FZF_DEFAULT_COMMAND}"
+
+# Use fd (https://github.com/sharkdp/fd) instead of the default find
+# command for listing path candidates.
+# - The first argument to the function ($1) is the base path to start traversal
+# - See the source code (completion.{bash,zsh}) for the details.
+_fzf_compgen_path() {
+  fd --hidden --follow --exclude ".git" . "$1"
+}
+
+# Use fd to generate the list for directory completion
+_fzf_compgen_dir() {
+  fd --type d --hidden --follow --exclude ".git" . "$1"
+}
+
+export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+
+bind '"\C-x": "\C-x\C-addi`__fzf_cd__`\C-x\C-e\C-x\C-r\C-m"'
+bind -m vi-command '"\C-x": "ddi`__fzf_cd__`\C-x\C-e\C-x\C-r\C-m"'
