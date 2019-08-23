f77f769cd9a4b3814039f72db36e9e17bdde89b0
Jon Nalley <code@bluebot.org>
2019-08-22 20:39:50 -0500

    Still need to source nvm.sh

--- bash.d/hooks/nvm.sh
+++ bash.d/hooks/nvm.sh
@@ -3,3 +3,13 @@
 [[ -s ~/.nvm/alias/default ]] || return 1
 
 export PATH=~/.nvm/versions/node/v$(<~/.nvm/alias/default)/bin:${PATH}
+
+[[ -s "${HOMEBREW_PREFIX}/opt/nvm/nvm.sh" ]] || return 1
+
+export NVM_DIR=~/.nvm
+
+nvm() {
+  unset nvm
+  source "${HOMEBREW_PREFIX}/opt/nvm/nvm.sh"
+  nvm "$@"
+}
