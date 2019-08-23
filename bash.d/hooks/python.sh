4838f39d27e32bb12a8c293467c1f8b97008d26c
Jon Nalley <code@bluebot.org>
2019-08-22 19:10:01 -0500

    Get rid of python2

--- bash.d/hooks/python.sh
+++ bash.d/hooks/python.sh
@@ -1,25 +1,14 @@
 # vim: set ft=sh:ts=4:sw=4:noet:nowrap # bash
 
-: "${VENV_BASE_DIR:=${HOME}/local}"
+export PYTHONBREAKPOINT=ipdb.set_trace
+export PATH=${HOME}/.virtualenv/bin:${PATH}
 
 python_reset() {
-  local p
-  rm -rf "${VENV_BASE_DIR}"/python[2-3]
-  for p in python2 python3; do
-    "${HOME}/.bash.d/scripts/create_python_venv.sh" "${p}"
-  done
+  echo "Creating ${HOME}/.virtualenv..."
+  python3 -m venv "${HOME}/.virtualenv" && hash -r &&
+    python3 -m pip install --upgrade pip &&
+    [[ -s ~/.requirements.txt ]] &&
+    python3 -m pip install --requirement ~/.requirements.txt
 }
 
-for p in $(shopt -s nullglob && echo "${VENV_BASE_DIR}"/python[2-3]); do
-  [[ -x ${p}/bin/python ]] || continue
-  export PATH=${p}/bin:${PATH}
-  p=${p##*/}
-  export "${p^^}"="$(command -v "${p}")"
-done
-
-unset p
-
-if ~/.bash.d/scripts/package_installed.py ipdb; then
-  export PYTHONBREAKPOINT=ipdb.set_trace
-  alias pydebug="PYTHONBREAKPOINT=ipdb.set_trace python -m ipdb -c continue"
-fi
+[[ -d "${HOME}/.virtualenv" ]] || python_reset
