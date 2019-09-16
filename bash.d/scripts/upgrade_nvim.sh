5530afe78a97a36d5c269bc9f12c68be4523fa70
Jon Nalley <code@bluebot.org>
2019-09-15 23:04:29 -0500

    Added script to install latest neovim

---
+++ bash.d/scripts/upgrade_nvim.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+url=https://github.com/neovim/neovim/releases/download/stable
+
+die() { echo "$*" && exit 1; }
+
+case "$(uname -o)" in
+  Darwin)
+    filename=nvim-macos.tar.gz
+    ;;
+  GNU/Linux)
+    filename=nvim-linux64.tar.gz
+    ;;
+  *)
+    die "Unsupported OS!"
+    ;;
+esac
+
+curl -sSL "${url}/${filename}" |
+  tar -C "${HOME}/local" -x --strip-components=1 -f -
