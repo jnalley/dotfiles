d6c75c78c1edc5a7af13ada743e2cbce41773144
Jon Nalley <code@bluebot.org>
2020-04-02 18:54:24 -0500

    Refactoring Hammerspoon

--- hammerspoon/Spoons/TapCtrlForEscape.spoon/init.lua
+++ hammerspoon/Spoons/TapCtrlForEscape.spoon/init.lua
@@ -1,19 +1,5 @@
--- Send 'ESCAPE' when 'CTRL' is pressed and released.
--- Other 'CTRL' combinations work as expected.
---
--- Originally based on:
---   https://gist.github.com/zcmarine/f65182fe26b029900792fa0b59f09d7f
---
--- NOTE: This plugin depends on being able to monitor the state of non-modifier
--- keys.  Mac OS X has a "Secure Input Mode", used in password dialogs, that
--- prevents applications from monitoring keyboard input.  When secure input
--- mode is enabled, a red keyboard icon will be displayed in the menubar.
---
--- See:
--- https://wiki.keyboardmaestro.com/Troubleshooting#Secure_Input_Mode
--- https://smilesoftware.com/textexpander/secureinput
-
 local obj = {}
+obj.__index = obj
 
 obj.name = "TapCtrlForEscape"
 obj.version = "0.1"
@@ -21,47 +7,13 @@ obj.author = "Jon Nalley <code@bluebot.org>"
 obj.homepage = "https://github.com/Hammerspoon/Spoons"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
+obj.logger = hs.logger.new("TapCtrlForEscape")
 obj.menubar = nil
 
-local CTRL = hs.eventtap.event.rawFlagMasks["control"]
-local RIGHT_CTRL = hs.eventtap.event.rawFlagMasks["deviceRightControl"]
-local NON_COALESCED = hs.eventtap.event.rawFlagMasks["nonCoalesced"]
-
--- raw flags that are set for internal keyboard
-local BUILTIN = CTRL | RIGHT_CTRL | NON_COALESCED
-
-local function ctrl_key_up(evt)
-  return next(evt:getFlags()) == nil and evt:getProperty(
-    hs.eventtap.event.properties.keyboardEventKeycode
-  ) == 62
-end
-
-local function abort()
-  if hs.eventtap.isSecureInputEnabled() then
-    enable_secure_mode_indicator()
-  else
-    disable_secure_mode_indicator()
-  end
-  return obj.non_modifier_tap:isEnabled() and obj.non_modifier_tap:stop() and true
-end
-
-local function modifier_handler(evt)
-  -- skip events for external keyboard
-  if evt:rawFlags() ~= BUILTIN then
+local function enable_secure_mode_indicator()
+  if obj.menubar ~= nil then
     return
   end
-
-  if evt:getFlags():containExactly({"ctrl"}) then
-    obj.non_modifier_tap:start() -- ESCAPE pending
-  elseif abort() and ctrl_key_up(evt) then
-    hs.eventtap.keyStroke({}, "ESCAPE", 3000)
-  end
-end
-
-function obj.init()
-end
-
-function obj.enable_secure_mode_indicator()
   obj.menubar = hs.menubar.new()
   obj.menubar:setTooltip("Secure input is inhibiting TapCtrlForEscape")
   obj.menubar:setIcon(
@@ -71,50 +23,67 @@ function obj.enable_secure_mode_indicator()
   )
 end
 
-function obj.disable_secure_mode_indicator()
-  if obj.menubar then
+local function disable_secure_mode_indicator()
+  if obj.menubar ~= nil then
     obj.menubar:delete()
     obj.menubar = nil
   end
 end
 
---- TapCtrlForEscape:start()
---- Method
---- Starts TapCtrlForEscape
----
---- Parameters:
----  * None
----
---- Returns:
----  * The TapCtrlForEscape object
-function obj.start()
+local function abort()
+  return (
+      obj.non_modifier_tap:isEnabled() and
+      obj.non_modifier_tap:stop() and true
+    )
+end
+
+local function ctrl_key_up(evt)
+  return next(evt:getFlags()) == nil and evt:getProperty(
+    hs.eventtap.event.properties.keyboardEventKeycode
+  ) == 62
+end
+
+local function modifier_handler(evt)
+  -- if hs.eventtap.isSecureInputEnabled() then
+  --   obj.logger.w("Secure input is enabled - TapCtrlForEscape will fail!")
+  --   enable_secure_mode_indicator()
+  -- else
+  --   disable_secure_mode_indicator()
+  -- end
+  if evt:getFlags():containExactly({"ctrl"}) and not hs.eventtap.isSecureInputEnabled() then
+    obj.logger.d("ESCAPE pending")
+    obj.non_modifier_tap:start()
+  elseif abort() and ctrl_key_up(evt) then
+    obj.logger.d("sending ESCAPE")
+    hs.eventtap.keyStroke({}, "ESCAPE", 3000)
+  end
+end
+
+function obj:init()
+end
+
+function obj:start()
   -- isEnabled() is used as a "flag" for a pending ESCAPE
-  obj.non_modifier_tap = hs.eventtap.new(
+  self.non_modifier_tap = hs.eventtap.new(
     {hs.eventtap.event.types.keyDown},
     function() obj.non_modifier_tap:stop() end
   )
 
   -- modifier key presses
-  obj.modifier_tap = hs.eventtap.new(
+  self.modifier_tap = hs.eventtap.new(
     {hs.eventtap.event.types.flagsChanged}, modifier_handler
   ):start()
 
-  return obj
+  return self
 end
 
---- TapCtrlForEscape:stop()
---- Method
---- Stops TapCtrlForEscape
----
---- Parameters:
----  * None
----
---- Returns:
----  * The TapCtrlForEscape objectini
-function obj.stop()
-  obj.disable_secure_mode_indicator()
-  obj.non_modifier_tap:stop()
-  obj.modifier_tap:stop()
+function obj:stop()
+  for tap in ipairs({"non_modifier_tap", "modifier_tap"}) do
+    if self[tap] ~= nil then
+      self[tap]:stop()
+    end
+    self[tap] = nil
+  end
 end
 
 return obj
--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,25 +2,75 @@
 math.randomseed(os.time())
 
 -- default log level
-hs.logger.defaultLogLevel = 'info'
-
--- load plugins
-require("plugins")
-
--- weather
--- require("hs-weather").start{ geolocation = true, units = "F" }
+hs.logger.defaultLogLevel = "info"
 
 -- disable icons
 hs.dockIcon(false)
-hs.menuIcon(false)
+hs.menuIcon(true)
 
--- disable window animations
+-- set console colors
+hs.console.outputBackgroundColor(hs.drawing.color.ansiTerminalColors.bgBlack)
+hs.console.consoleCommandColor(hs.drawing.color.ansiTerminalColors.fgGreen)
+
+-- only reload if a .lua file changed
+local function reload(files)
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      hs.reload()
+    end
+  end
+end
+
+local pw = hs.pathwatcher.new(hs.configdir, reload):start()
+
+-- a work-around for websites that disable paste
+hs.hotkey.bind({"cmd", "ctrl"}, "v", function()
+  hs.eventtap.keyStrokes(hs.pasteboard.getContents())
+end)
+
+hs.grid.setGrid("2x2")
+hs.grid.ui.showExtraKeys = true
+hs.hotkey.bind({"alt", "cmd"}, "G", function() hs.grid.show() end)
+
+hs.hints.style = "vimperator"
 hs.window.animationDuration = 0
+
+-- window management hotkeys
+local function moveToUnit(unit)
+  hs.window.focusedWindow():moveToUnit(unit)
+end
+
+hs.hotkey.bind({"alt", "cmd"}, "Left",
+  hs.fnutils.partial(moveToUnit, hs.layout.left50)
+  -- 50% vertical
+  -- hs.fnutils.partial(moveToUnit, hs.geometry.unitrect(0,0,0.5,0.5))
+)
+hs.hotkey.bind({"alt", "cmd"}, "Right",
+  hs.fnutils.partial(moveToUnit, hs.layout.right50)
+)
+hs.hotkey.bind({"alt", "cmd"}, "Up",
+  function() hs.window.frontmostWindow():maximize() end
+)
+
+-- toggles inhibit of system sleep
+hs.hotkey.bind({"cmd", "alt"}, "s", function()
+  hs.alert.show(
+    hs.caffeinate.toggle("displayIdle") and
+    "Sleep disabled" or "Normal sleep restored"
+  )
+end)
 
 -- cli
 -- if not hs.ipc.cliStatus() then
 --   hs.ipc.cliInstall()
 -- end
 
+-- local hammerspoon = hs.application.open("hammerspoon")
+-- hs.timer.doAfter(3, function() print(hammerspoon:bundleID()) end)
+
+hs.loadSpoon("TapCtrlForEscape"):start()
+
 -- done
-hs.alert.show("Hammerspoon loaded")
+FadeLogo = hs.loadSpoon("FadeLogo")
+FadeLogo.run_time = 0.8
+FadeLogo:start()
