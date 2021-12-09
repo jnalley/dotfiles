6357f402ad878ae363ce72720616c5df1d2d2c5a
Jon Nalley <code@bluebot.org>
2021-12-08 23:40:52 -0600

    Remove lspinstall

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -1,11 +1,5 @@
 local vim = vim
 local lspconfig = require "lspconfig"
-local lspinstall = require "lspinstall"
-
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
-lspinstall.setup()
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
   vim.lsp.with(
@@ -18,80 +12,58 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] =
   }
 )
 
-local settings = {
-  efm = {
-    init_options = {documentFormatting = true},
-    root_dir = vim.loop.cwd,
-    filetypes = {
-      "python",
-      "sh"
-    },
-    settings = {
-      languages = {
-        python = {
-          {
-            formatCommand = "black --line-length 79 -",
-            formatStdin = true
-          },
-          {
-            lintCommand = "flake8 --ignore=E203,E266,E501,W503 --per-file-ignores=__init__.py:F401 --max-line-length 79 --format '%(path)s:%(row)d:%(col)d: %(code)s %(code)s %(text)s' --stdin-display-name ${INPUT} -",
-            lintStdin = true,
-            lintIgnoreExitCode = true,
-            lintFormats = {"%f:%l:%c: %t%n%n%n %m"},
-            lintSource = "flake8"
-          },
-          {
-            formatCommand = "isort --stdout --profile black -",
-            formatStdin = true
-          }
+lspconfig["efm"].setup{
+  init_options = {documentFormatting = true},
+  root_dir = vim.loop.cwd,
+  filetypes = {
+    "python",
+    "sh",
+    "yaml"
+  },
+  settings = {
+    languages = {
+      python = {
+        {
+          formatCommand = "black --line-length 79 -",
+          formatStdin = true
         },
-        sh = {
-          {
-            lintCommand = "shellcheck -f gcc -x -",
-            lintStdin = true,
-            lintFormats = {
-              "%f:%l:%c: %trror: %m",
-              "%f:%l:%c: %tarning: %m",
-              "%f:%l:%c: %tote: %m"
-            },
-            lintSource = "shellcheck"
+        {
+          lintCommand = "flake8 --ignore=E203,E266,E501,W503 --per-file-ignores=__init__.py:F401 --max-line-length 79 --format '%(path)s:%(row)d:%(col)d: %(code)s %(code)s %(text)s' --stdin-display-name ${INPUT} -",
+          lintStdin = true,
+          lintIgnoreExitCode = true,
+          lintFormats = {"%f:%l:%c: %t%n%n%n %m"},
+          lintSource = "flake8"
+        },
+        {
+          formatCommand = "isort --stdout --profile black -",
+          formatStdin = true
+        }
+      },
+      sh = {
+        {
+          lintCommand = "shellcheck -f gcc -x -",
+          lintStdin = true,
+          lintFormats = {
+            "%f:%l:%c: %trror: %m",
+            "%f:%l:%c: %tarning: %m",
+            "%f:%l:%c: %tote: %m"
           },
-          {
-            formatCommand = "shfmt ${-i:tabWidth} -ci"
-          }
+          lintSource = "shellcheck"
+        },
+        {
+          formatCommand = "shfmt -ci -i 2 -s -bn",
+          formatStdin = true
+        }
+      },
+      yaml = {
+        {
+          lintCommand = "yamllint -f parsable -",
+          lintStdin = true
         }
       }
     }
   }
 }
 
-settings["python"] = {
-  filetypes = { 'python' },
-  settings = {
-    python = {
-      analysis = {
-        autoSearchPaths = true,
-        autoImportCompletions = true,
-        useLibraryCodeForTypes = true,
-      },
-    },
-  },
-}
-
-local manual = {"efm"}
-local servers = vim.list_extend(lspinstall.installed_servers(), manual)
-
-for _, server in ipairs(servers) do
-  if lspconfig[server] ~= nil then
-    local s = settings[server] or {}
-    s["capabilities"] = capabilities
-    lspconfig[server].setup(s)
-  else
-    vim.notify(
-      string.format("Unable to enable language server: %s", server),
-      "ERROR"
-    )
-  end
-end
-
 lspconfig["jedi_language_server"].setup{}
+lspconfig["solargraph"].setup{}
